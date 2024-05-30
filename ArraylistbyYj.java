37168b55a0c499d8a4f29259e0e5b3c16f6a8de1
Thunderbolt17yj <122665067+Thunderbolt17yj@users.noreply.github.com>
2024-05-30 20:41:13 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ ArraylistbyYj.java
@@ -0,0 +1,16 @@
+package june;
+
+import java.util.ArrayList;
+
+public class ArraylistbyYj{
+    public static void main
+            (String[] args){
+        ArrayList<String>Games= new ArrayList<>();
+        Games.add("basketball");
+        Games.add("volleyball");
+        Games.add("cricket");
+        System.out.print(Games);
+        System.out.println(Games.get(2));
+    }
+
+}
